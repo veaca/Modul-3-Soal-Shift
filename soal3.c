4dcefaaaacb077e0d565ce701842e5121ba27b1d
veaca <veaca13@yahoo.com>
2017-10-24 20:57:02 +0700

    [No3] Baru fungsi darah lohan

--- soal3.c
+++ soal3.c
@@ -1,13 +1,30 @@
 #include <string.h>
 #include <stdio.h>
-//#include <pthread.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int lohan =100;
+int kepiting = 100;
+
+void *MinusLohan (void *ptr){
+	int i, length=10;
+	for (int i=0; i<=10; i++){
+//		printf ("%d ", i);
+		sleep(1);
+	}
+	lohan -= 15;
+}
 
 int main(){
-	int lohan, kepiting;
-	lohan = 100;
-	kepiting = 100;
-
-	while (1){
+//	int lohan, kepiting;
+//	lohan = 100;
+//	kepiting = 100;
+	int err;
+	pthread_t tid[3];
+//	while (lohan >0 && kepiting >0)err = pthread_create (&(tid[1]), NULL, MinusLohan, NULL);
+	while (lohan > 0 && kepiting > 0){
+		err = pthread_create (&(tid[1]), NULL, MinusLohan, NULL);
 		printf ("---Pilih fitur---\n1. Beri makan lohan \n2. Beri makan kepiting\n");
 		printf ("3. Lihat status\n");
 		printf ("Masukkan fitur (1/2/3)\n");
@@ -17,6 +34,10 @@ int main(){
 		else if (fitur == 2) kepiting += 10;
 		else if (fitur == 3) {
 			printf ("Lohan = %d\nKepiting =%d\n\n", lohan , kepiting);
+		}
+		if (lohan < 0 || kepiting < 0){
+		printf ("Game Selesai\n");
+		break;
 		}
 	}
 }
