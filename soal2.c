da429df7eb18a2f270bfbb70044d497a54bf3db1
veaca <veaca13@yahoo.com>
2017-10-25 00:24:34 +0700

    [No2] suda ada mutex tinggal beresin

--- soal2.c
+++ soal2.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+int status1;
 char pemain1[100];
 char pemain2[100];
 int kotak1[16];
@@ -14,42 +15,48 @@ int poin1=0;
 int poin2=0;
 
 void *gerak1 (void *ptr){
+	status1=0;
 	int jumlah;
 	printf ("Masukkan jumlah ranjau yang ingin dipasang: ");
 	scanf ("%d", &jumlah);
-	for (int i=1;i<=4;i++){
+	for (int i=1;i<=jumlah;i++){
 		int pos;
-		printf ("Ranjau %d: ", i);
+		printf ("Ranjau %d: Posisi ", i);
 		scanf ("%d", &pos);
 		kotak1[pos]=1;
 	}
+	status1=1;
 }
 
 void *gerak2 (void *ptr){
+	status1=0;
         int jumlah;
         printf ("Masukkan jumlah ranjau yang ingin dipasang: ");
         scanf ("%d", &jumlah);
-        for (int i=1;i<=4;i++){
+        for (int i=1;i<=jumlah;i++){
                 int pos;
-                printf ("Ranjau %d: ", i);
+                printf ("Ranjau %d: Posisi ", i);
                 scanf ("%d", &pos);
                 kotak2[pos]=1;
         }
+	status1=1;
 }
 
 void *tebak1 (void *ptr){
+	while (status1!=1){}
         for (int i=1;i<=4;i++){
                 int pos;
-                printf ("Tebakan %d: ", i);
+                printf ("Tebakan %d: Posisi ", i);
                 scanf ("%d", &pos);
                 tebakan1[pos]=1;
         }
 }
 
 void *tebak2 (void *ptr){
+	while (status1!=1) {}
         for (int i=1;i<=4;i++){
                 int pos;
-                printf ("Tebakan %d: ", i);
+                printf ("Tebakan %d: Posisi ", i);
                 scanf ("%d", &pos);
                 tebakan2[pos]=1;
         }
@@ -71,18 +78,18 @@ void cek2(){
         for (int i=1;i<=16;i++) tebakan1[i]=0;
 }
 
-bool keluar1(){
+int keluar1(){
 	for (int i=1;i<=16;i++) {
-		if (kotak1[i]==0) return false;
+		if (kotak1[i]==0) return 0;
 	}
-	return true;
+	return 1;
 }
 
-bool keluar2(){
+int keluar2(){
         for (int i=1;i<=16;i++) {
-                if (kotak2[i]==0) return false;
+                if (kotak2[i]==0) return 0;
         }
-        return true;
+        return 1;
 }
 int main(){
 	int move1, guess1,move2,guess2, gerak=1;
@@ -93,22 +100,30 @@ int main(){
 	pthread_t tid[4];
 	while (1){
 		if (gerak==1){
+			printf ("Pemain %s : Masukkan ranjau\n", pemain1);
+			status1=0;
 			move1 = pthread_create (&tid[1], NULL, gerak1, NULL);
+			if (status1==1) printf ("Pemain %s : Menebak\n", pemain2);
 			guess2 = pthread_create (&tid[4], NULL, tebak2, NULL);
 			pthread_join (tid[1], NULL);
 			pthread_join (tid[4], NULL);
 			cek1();
 		gerak=2;
+		system("clear");
 		}
 		if (gerak==2){
+			printf ("Pemain %s : Masukkan ranjau\n", pemain2);
+			status1=0;
                         move2 = pthread_create (&tid[3], NULL, gerak1, NULL);
+			if (status1==1) printf ("Pemain %s : Menebak\n", pemain1);
                         guess1 = pthread_create (&tid[2], NULL, tebak2, NULL);
                         pthread_join (tid[3], NULL);
                         pthread_join (tid[2], NULL);
 			cek2();
                 gerak=1;
+		system("clear");
                 }
-		if (poin1 ==10 || poin2==10 || keluar1 || keluar2 )break;
+		if (poin1 ==10 || poin2==10 || keluar1()==1 || keluar2()==1 )break;
 
 /*		char input[100];
 		scanf ("%s",input);
