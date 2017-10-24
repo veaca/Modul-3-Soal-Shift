67094aa8756186f1f9091e2c51ee8ef958444878
veaca <veaca13@yahoo.com>
2017-10-24 21:09:20 +0700

    [No3] Sudah fungsi minus lohan sama kepitingnya

--- soal3.c
+++ soal3.c
@@ -3,28 +3,41 @@
 #include <pthread.h>
 #include <stdlib.h>
 #include <unistd.h>
-
+int stat_lohan;
+int stat_kepiting;
 int lohan =100;
 int kepiting = 100;
 
 void *MinusLohan (void *ptr){
 	int i, length=10;
+	while (stat_lohan==0){
 	for (int i=0; i<=10; i++){
 //		printf ("%d ", i);
 		sleep(1);
 	}
-	lohan -= 15;
+	lohan -= 15;}
 }
+void *MinusKepiting (void *ptr){
+	int i, length=20;
+	while (stat_kepiting==0){
+	for (int i=0;i<=length;i++) sleep (1);
+	kepiting -= 10;}
+}
+
 
 int main(){
 //	int lohan, kepiting;
 //	lohan = 100;
 //	kepiting = 100;
-	int err;
+	int err1, err2;
 	pthread_t tid[3];
-//	while (lohan >0 && kepiting >0)err = pthread_create (&(tid[1]), NULL, MinusLohan, NULL);
-	while (lohan > 0 && kepiting > 0){
-		err = pthread_create (&(tid[1]), NULL, MinusLohan, NULL);
+	stat_lohan=0;
+	stat_kepiting=0;
+	err1 = pthread_create (&(tid[1]), NULL, MinusLohan, NULL);
+	err2 = pthread_create (&(tid[2]), NULL, MinusKepiting, NULL);
+//	for (int i=1;i<=2;i++) pthread_join (tid[i],NULL);
+	while (1){
+		//err = pthread_create (&(tid[1]), NULL, MinusLohan, NULL);
 		printf ("---Pilih fitur---\n1. Beri makan lohan \n2. Beri makan kepiting\n");
 		printf ("3. Lihat status\n");
 		printf ("Masukkan fitur (1/2/3)\n");
@@ -36,6 +49,7 @@ int main(){
 			printf ("Lohan = %d\nKepiting =%d\n\n", lohan , kepiting);
 		}
 		if (lohan < 0 || kepiting < 0){
+		stat_lohan=1;
 		printf ("Game Selesai\n");
 		break;
 		}
