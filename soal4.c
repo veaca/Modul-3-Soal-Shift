7161e81a35f019fc0cc0792840bd73e69d2f28a4
veaca <veaca13@yahoo.com>
2017-10-24 19:57:51 +0700

    [No4] sudah jadi multithreadnya tinggal beresin kodingan

--- soal4.c
+++ soal4.c
@@ -1,17 +1,44 @@
+#include <pthread.h>
 #include <stdio.h>
+int n[100];
+int status;
 
-int faktorial (int n){
+void *faktorial (void *ptr){
+	status=0;
+	int *x=ptr;
 	int hasil=1;
-	for (int i=1;i<=n;i++){
+//	status =1;
+//	printf ("%d\n", *x);
+	for (int i=1;i<=n[*x];i++){
 		hasil = hasil*i;
 	}
-	return hasil;
+	printf("%d\n", hasil);
+	status=1;
+	return 0;
 }
 
 int main(){
-	int n;
+	int lebar;
+	printf ("Masukkan lebar: ");
+	scanf ("%d", &lebar);
+//	int n[lebar];
 	printf ("Masukkan angka: ");
-	scanf ("%d", &n);
-	printf ("faktorialnya: %d\n", faktorial (n));
+	for (int i=1;i<=lebar;i++){
+	scanf ("%d", &n[i]);
+	}
+	int err;
+	pthread_t tid[lebar+1];
+	int pos=1;
+	while (pos<=lebar){
+	status=0;
+	printf ("%d: ", pos);
+	err=pthread_create (&(tid[pos]), NULL, faktorial, (void*)&pos);
+//	printf ("faktorialnya: %d\n", faktorial (n));
+	while (status!=1) {}
+	pos++;
+	}
 
+	for (int x=1;x<=lebar;x++){
+	pthread_join (tid[x], NULL);
+	}
 }
