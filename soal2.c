1dd96f268209bd6d5bbe39b28a3790d0e65b5202
veaca <veaca13@yahoo.com>
2017-10-24 23:45:47 +0700

    [No2] sudah jadi smua fungsinya tinggal mutexnya

--- soal2.c
+++ soal2.c
@@ -55,16 +55,49 @@ void *tebak2 (void *ptr){
         }
 }
 
+void cek1(){
+	for (int i=1;i<=16;i++){
+		if (tebakan2[i]==1 && kotak1[i]==1) poin1++;
+		else if (tebakan2[i]==1 && kotak1[i]!=1) poin2++;
+	}
+	for (int i=1;i<=16;i++) tebakan2[i]=0;
+}
+
+void cek2(){
+        for (int i=1;i<=16;i++){
+                if (tebakan1[i]==1 && kotak2[i]==1) poin2++;
+                else if (tebakan1[i]==1 && kotak2[i]!=1) poin1++;
+        }
+        for (int i=1;i<=16;i++) tebakan1[i]=0;
+}
 
 
 int main(){
+	int move1, guess1,move2,guess2, gerak=1;
 	printf ("Masukkan nama pemain 1: ");
 	scanf ("%s",pemain1);
         printf ("Masukkan nama pemain 2: ");
 	scanf ("%s",pemain2);
+	pthread_t tid[4];
 	while (1){
-		char input[100];
+		if (gerak==1){
+			move1 = pthread_create (&tid[1], NULL, gerak1, NULL);
+			guess2 = pthread_create (&tid[4], NULL, tebak2, NULL);
+			pthread_join (tid[1], NULL);
+			pthread_join (tid[4], NULL);
+			cek1();
+		gerak=2;
+		}
+		if (gerak==2){
+                        move2 = pthread_create (&tid[3], NULL, gerak1, NULL);
+                        guess1 = pthread_create (&tid[2], NULL, tebak2, NULL);
+                        pthread_join (tid[3], NULL);
+                        pthread_join (tid[2], NULL);
+			cek2();
+                gerak=1;
+                }
+/*		char input[100];
 		scanf ("%s",input);
-		if (strcmp("Cek", input) == 0) printf ("Poin %s : %d\nPoin %s : %d", pemain1, poin1, pemain2, poin2); 
+		if (strcmp("Cek", input) == 0) printf ("Poin %s : %d\nPoin %s : %d", pemain1, poin1, pemain2, poin2); */
 	}
 }
