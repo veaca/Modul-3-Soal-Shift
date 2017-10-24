ccdde7aaefd888e42568ab86f116dbb3cf04c503
veaca <veaca13@yahoo.com>
2017-10-24 23:23:18 +0700

    [No2] tambah nama dan fungsi gerak

--- soal2.c
+++ soal2.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <string.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 char pemain1[100];
 char pemain2[100];
 int kotak1[16];
@@ -9,10 +13,26 @@ int tebakan2[16];
 int poin1=0;
 int poin2=0;
 
+void *gerak1 (void *ptr){
+	int jumlah;
+	printf ("Masukkan jumlah ranjau yang ingin dipasang: ");
+	scanf ("%d", &jumlah);
+	for (int i=1;i<=4;i++){
+		int pos;
+		printf ("Ranjau %d: ", i);
+		scanf ("%d", &pos);
+		kotak1[pos]=1;
+	}
+}
+
 int main(){
 	printf ("Masukkan nama pemain 1: ");
-	scanf ("%s", &pemain1);
+	scanf ("%s",pemain1);
         printf ("Masukkan nama pemain 2: ");
-	scanf ("%s", &pemain2);
-
+	scanf ("%s",pemain2);
+	while (1){
+		char input[100];
+		scanf ("%s",input);
+		if (strcmp("Cek", input) == 0) printf ("Poin %s : %d\nPoin %s : %d", pemain1, poin1, pemain2, poin2); 
+	}
 }
