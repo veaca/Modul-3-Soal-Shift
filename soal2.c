6d3d3baeacaf234bc3b312920aa6f14bea970fde
veaca <veaca13@yahoo.com>
2017-10-24 22:01:44 +0700

    [No2] baru deklarasinya

---
+++ soal2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+char pemain1[100];
+char pemain2[100];
+int kotak1[16];
+int kotak2[16];
+int tebakan1[16];
+int tebakan2[16];
+int poin1=0;
+int poin2=0;
+
+int main(){
+	printf ("Masukkan nama pemain 1: ");
+	scanf ("%s", &pemain1);
+        printf ("Masukkan nama pemain 2: ");
+	scanf ("%s", &pemain2);
+
+}
