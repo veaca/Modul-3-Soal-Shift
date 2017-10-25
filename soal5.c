4423d3cb5481ed7df1c6017b2bd164bb40e12f92
baguztaji07 <baguztaji07@gmail.com>
2017-10-25 08:05:52 +0700

    Hapus kodingan tidak perlu

--- soal5.c
+++ soal5.c
@@ -22,15 +22,11 @@ darifile h2h2[2000];
 int z,r,t,b=0,j=0;
 void* caridong(void *arg){
 	pthread_t id=pthread_self();
-//	printf("berhasil caridong\n");
 		if(pthread_equal(id,tid[0])){
 			status=0;
-//			printf("berhasil pthread count 0\n");
 			for(z=0;z<=b;z++){
-//				printf("berhasil masuk z %d loop\n",z);
 				if(strcmp(orang[0].nama,h2h2[z].perkata)==0){
 					banyaknya[0]=banyaknya[0]+1;
-//					printf("Berhasil masuk banyak kata%d \n",banyaknya[0]);
 				}
 			}
 			printf("%s : %d\n",orang[0].nama,banyaknya[0]);
@@ -38,12 +34,9 @@ void* caridong(void *arg){
 		}
 		if(pthread_equal(id,tid[1])){
                       while(status!=1){ }
-  //                    printf("berhasil pthread count 1\n");
                         for(r=0;r<=b;r++){
- //                             printf("berhasil masuk r %d loop\n",r);
                                 if(strcmp(orang[1].nama,h2h2[r].perkata)==0){
                                         banyaknya[1]=banyaknya[1]+1;
-   //                                   printf("Berhasil masuk banyak kata%d \n",banyaknya[1]);
                                 }
                         }
                         printf("%s : %d\n",orang[1].nama,banyaknya[1]);
@@ -52,12 +45,9 @@ void* caridong(void *arg){
                 }
 		if(pthread_equal(id,tid[2])){
                       while(status!=2){ }
-     //                 printf("berhasil pthread count 2\n");
                         for(t=0;t<=b;t++){
-       //                       printf("berhasil masuk t %d loop\n",t);
                                 if(strcmp(orang[2].nama,h2h2[t].perkata)==0){
                                         banyaknya[2]=banyaknya[2]+1;
-         //                             printf("Berhasil masuk banyak kata%d \n",banyaknya[2]);
                                 }
                         }
                         printf("%s : %d\n",orang[2].nama,banyaknya[2]);
@@ -71,17 +61,10 @@ int main(){
 	int i=0,k=0,a=0,c=0;
 	scanf("%s %[^\n]",&hehe,&input);
 	for(i=0;i<=strlen(input);i++){
-                //cari[j][i]=input[i];
-               // orang[j].nama[k]=input[i];
-//                printf("%c\n",orang[j].nama[k]);
-               // k++;
                 if(input[i]==32){
-//                      printf("coba\n");
-//                        pthread_create(&(tid[j]),NULL,&caridong,NULL);
                         j=j+1;
                         k=0;
 			continue;
-//                      printf("berhasil\n");
                 }
 		orang[j].nama[k]=input[i];
 		k++;
@@ -90,36 +73,23 @@ int main(){
         novel = fopen("novel.txt", "r");
         fscanf(novel,"%[^\n]",&h3h3);
 	for(a=0;a<=strlen(h3h3);a++){
-                //h2h2[b].perkata[c]=h3h3[a];
-                //c++;
                 if(h3h3[a]==32){
-                        //pthread_create(&(tid[j]),NULL,&caridong,NULL);
                         b=b+1;
                         c=0;
 			continue;
-  //                      printf("berhasil\n");
                 }
 		if(h3h3[a]==44) continue;
+		if(h3h3[a]==46) continue;
 		h2h2[b].perkata[c]=h3h3[a];
 		c++;
         } 
 	fclose(novel);
 
-	for(haha=0;haha<=j;haha++){
-//		printf("%d",haha);
+	for(haha=0;haha<=j;haha){
 		pthread_create(&(tid[haha]),NULL,&caridong,NULL);
 	}
 	pthread_join(tid[0], NULL);
      	pthread_join(tid[1], NULL);
 	pthread_join(tid[2], NULL);
-/*	for(haha=0;haha<=b;haha++){
-		printf("%s\n",orang[0].nama);
-		printf("%s ",h2h2[haha].perkata);
-                if(strcmp(orang[0].nama,h2h2[haha].perkata)==0){
-                        banyaknya[count]=banyaknya[count]+1;
-                        printf("Berhasil masuk banyak kata%d \n",banyaknya[count]);
-                }
-                //printf("%d",banyaknya[count]);
-        }
-*/	return 0;
+	return 0;
 }
