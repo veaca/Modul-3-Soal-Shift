0517cf2c0ce975aff5935c7cc4b53a04d4229a08
Bagus Aji <baguztaji07@gmail.com>
2017-10-25 14:19:38 +0700

    [No1server] fungsi tambah senjata

--- soal1s.c
+++ soal1s.c
@@ -12,9 +12,14 @@ struct shm{
 	int stok;
 };
 
+void lihat(){
+	int a,b,c;
+	//for(a=0;a<=
+}
+int xx=0;
 int main(){
-	int i,j=0,k=0;
-	char temp[1000];
+	int i,j=0,k=0,berapa;
+	char temp[20],temp1[20];
 	int shmid;
 	key_t key = 4567;
 	size_t size = 30;
@@ -26,11 +31,32 @@ int main(){
 		return -1;
 	}
 
-	if((shmdat[0] = shmat(shmid,NULL,0)) == (struct shm *) -1)
+	if((shmdat[0] = shmat(shmid,NULL,0)) == (struct shm*) -1)
 	{
 		printf("Errorn");
 		return -1;
 	}
-
+	
+	while(1){
+	//shmdat.tanda=1;
+	scanf("%s",&temp);
+	if (strcmp(temp,"LIHAT")==0){
+		scanf("%s",&temp1);
+		lihat();
+	}
+	else if (strcmp(temp,"EXIT")==0) break;
+	else {
+		scanf("%d",&berapa);
+		for(i=0;i<=xx;i++){
+			if(strcmp(temp,shmdat[i]->senjata)==0){
+				shmdat[i]->stok=shmdat[i]->stok+berapa;
+			}
+			else {
+				strcpy(shmdat[xx]->senjata,temp);
+				shmdat[xx]->stok=berapa;xx++;
+			}
+		}
+	}
+	}
 	return 0;
 }
