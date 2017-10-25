25bed6f8a8b15b948dbdcc1959928507198cd305
baguztaji07 <baguztaji07@gmail.com>
2017-10-25 07:57:23 +0700

    Sudah jadi thread dan fungsi tinggal rapikan

--- soal5.c
+++ soal5.c
@@ -8,6 +8,7 @@ char input[2000];
 char hehe[10];
 char h3h3[1000];
 char cari[20][20];
+int status=0;
 int haha,banyaknya[1000];
 pthread_t tid[10];
 typedef struct{
@@ -18,32 +19,52 @@ typedef struct{
 	char perkata[20];
 } darifile;
 darifile h2h2[2000];
-int z,b=0,j=0,count=0;
+int z,r,t,b=0,j=0;
 void* caridong(void *arg){
 	pthread_t id=pthread_self();
-	for(haha=0;haha<=b;haha++){
-                if(orang[0].nama==h2h2[haha].perkata){
-                        banyaknya[count]=banyaknya[count]+1;
-                	//printf("Berhasil masuk banyak kata%d \n",banyaknya[count]);
-                }
-		printf("%d",banyaknya[count]);
-        }
-
-	//printf("berhasil caridong\n");
-	/*while(count<=j){
-		if(pthread_equal(id,tid[count])){
-			printf("berhasil pthread count %d\n",count);
+//	printf("berhasil caridong\n");
+		if(pthread_equal(id,tid[0])){
+			status=0;
+//			printf("berhasil pthread count 0\n");
 			for(z=0;z<=b;z++){
-				printf("berhasil masuk z %d loop\n",z);
-				if(orang[count].nama==h2h2[z].perkata){
-					banyaknya[count]=banyaknya[count]+1;
-					printf("Berhasil masuk banyak kata%d \n",banyaknya[count]);
+//				printf("berhasil masuk z %d loop\n",z);
+				if(strcmp(orang[0].nama,h2h2[z].perkata)==0){
+					banyaknya[0]=banyaknya[0]+1;
+//					printf("Berhasil masuk banyak kata%d \n",banyaknya[0]);
 				}
 			}
-			printf("%s : %d\n",orang[count].nama,banyaknya[count]);
+			printf("%s : %d\n",orang[0].nama,banyaknya[0]);
+			status=status+1;
 		}
-		count++;
-	}*/
+		if(pthread_equal(id,tid[1])){
+                      while(status!=1){ }
+  //                    printf("berhasil pthread count 1\n");
+                        for(r=0;r<=b;r++){
+ //                             printf("berhasil masuk r %d loop\n",r);
+                                if(strcmp(orang[1].nama,h2h2[r].perkata)==0){
+                                        banyaknya[1]=banyaknya[1]+1;
+   //                                   printf("Berhasil masuk banyak kata%d \n",banyaknya[1]);
+                                }
+                        }
+                        printf("%s : %d\n",orang[1].nama,banyaknya[1]);
+                        
+                        status=status+1;
+                }
+		if(pthread_equal(id,tid[2])){
+                      while(status!=2){ }
+     //                 printf("berhasil pthread count 2\n");
+                        for(t=0;t<=b;t++){
+       //                       printf("berhasil masuk t %d loop\n",t);
+                                if(strcmp(orang[2].nama,h2h2[t].perkata)==0){
+                                        banyaknya[2]=banyaknya[2]+1;
+         //                             printf("Berhasil masuk banyak kata%d \n",banyaknya[2]);
+                                }
+                        }
+                        printf("%s : %d\n",orang[2].nama,banyaknya[2]);
+                        
+                        status=status+1;
+                }
+	
 
 }
 int main(){
@@ -56,7 +77,7 @@ int main(){
                // k++;
                 if(input[i]==32){
 //                      printf("coba\n");
-//                      pthread_create(&(tid[j]),NULL,&caridong,NULL);
+//                        pthread_create(&(tid[j]),NULL,&caridong,NULL);
                         j=j+1;
                         k=0;
 			continue;
@@ -84,17 +105,21 @@ int main(){
         } 
 	fclose(novel);
 
-/*	for(haha=0;haha<=j;haha++){
+	for(haha=0;haha<=j;haha++){
+//		printf("%d",haha);
 		pthread_create(&(tid[haha]),NULL,&caridong,NULL);
 	}
-*/	for(haha=0;haha<=b;haha++){
-//		printf("%s\n",orang[0].nama);
-//		printf("%s ",h2h2[haha].perkata);
+	pthread_join(tid[0], NULL);
+     	pthread_join(tid[1], NULL);
+	pthread_join(tid[2], NULL);
+/*	for(haha=0;haha<=b;haha++){
+		printf("%s\n",orang[0].nama);
+		printf("%s ",h2h2[haha].perkata);
                 if(strcmp(orang[0].nama,h2h2[haha].perkata)==0){
                         banyaknya[count]=banyaknya[count]+1;
                         printf("Berhasil masuk banyak kata%d \n",banyaknya[count]);
                 }
                 //printf("%d",banyaknya[count]);
         }
-	return 0;
+*/	return 0;
 }
