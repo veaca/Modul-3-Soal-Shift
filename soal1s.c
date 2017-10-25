4a4617050ca110a98f9ff37bbe2f0d77641002ef
Bagus Aji <baguztaji07@gmail.com>
2017-10-25 16:45:01 +0700

    [No1] ganti fungsi dan ganti type data, karena salah pemahaman soal

--- soal1s.c
+++ soal1s.c
@@ -6,56 +6,78 @@
 #include <stdlib.h>
 #define max 50
 
-struct shm{
-	char isi[max];
-	char senjata[max];
-	int stok;
-};
+int xx=0,*jumlah;
+struct dagang{
+	char senjata[8];
+}
+struct dagang nama[6];
+strcpy(nama[0].senjata,"MP4A1");
+strcpy(nama[1].senjata,"PM2-V1");
+strcpy(nama[2].senjata,"SPR-3");
+strcpy(nama[3].senjata,"SS2-V5");
+strcpy(nama[4].senjata,"SPG1-V3");
+strcpy(nama[5].senjata,"MINE");
 
 void lihat(){
-	int a,b,c;
-	//for(a=0;a<=
+	int a;
+	for(a=0;a<6;a++){
+		printf("%s %d\n",nama[a].senjata,total[a]);
+	}
 }
-int xx=0;
+void menu(){
+	system("clear");
+	printf("---MENU---\n");
+	printf("1.LIHAT STOK\n");
+	printf("2.TAMBAH STOK : [nama barang] [banyaknya]\n");
+}
 int main(){
 	int i,j=0,k=0,berapa;
-	char temp[20],temp1[20];
+	char temp[20],temp1[20],senjata[8];
 	int shmid;
 	key_t key = 4567;
 	size_t size = 30;
-	struct shm *shmdat[30];
 
 	if((shmid = shmget(key,size,IPC_CREAT | 0666)) == -1)
 	{
 		printf("Errorn");
 		return -1;
 	}
-
-	if((shmdat[0] = shmat(shmid,NULL,0)) == (struct shm*) -1)
-	{
-		printf("Errorn");
-		return -1;
-	}
 	
+	jumlah = shmat(shmid,NULL,0);
 	while(1){
 	//shmdat.tanda=1;
+	menu();
 	scanf("%s",&temp);
 	if (strcmp(temp,"LIHAT")==0){
 		scanf("%s",&temp1);
+		printf("masuk lihat\n");
 		lihat();
 	}
 	else if (strcmp(temp,"EXIT")==0) break;
-	else {
+	else if (strcmp(temp,"MP4A1"){ 
 		scanf("%d",&berapa);
-		for(i=0;i<=xx;i++){
-			if(strcmp(temp,shmdat[i]->senjata)==0){
-				shmdat[i]->stok=shmdat[i]->stok+berapa;
-			}
-			else {
-				strcpy(shmdat[xx]->senjata,temp);
-				shmdat[xx]->stok=berapa;xx++;
-			}
-		}
+		total[0]=total[0]+berapa;
+	}
+	else if (strcmp(temp,"PM2-V1"){ 
+                scanf("%d",&berapa);
+                total[1]=total[1]+berapa;
+        }
+	else if (strcmp(temp,"SPR-3"){ 
+                scanf("%d",&berapa);
+                total[2]=total[2]+berapa;
+        }
+	else if (strcmp(temp,"SS2-V5"){ 
+                scanf("%d",&berapa);
+                total[3]=total[3]+berapa;
+        }
+	else if (strcmp(temp,"SPG1-V3"){ 
+                scanf("%d",&berapa);
+                total[4]=total[4]+berapa;
+        }
+	else if (strcmp(temp,"MINE"){ 
+                scanf("%d",&berapa);
+                total[5]=total[5]+berapa;
+        }
 	}
 	}
 	return 0;
